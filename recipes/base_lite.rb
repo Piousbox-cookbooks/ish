61abb3c7a3a3eee757f964096265d6c498bd6a75
Victor Piousbox <piousbox@gmail.com>
2015-06-08 13:46:44 -0700

    grok

--- recipes/base_lite.rb
+++ recipes/base_lite.rb
@@ -1,22 +1,16 @@
 
-# ish base_lite
+#
+# Ish recipe base
+#
 
 execute 'apt-get update -y' do
   command %{apt-get update -y}
 end
 
-packages = %w{ gcc g++ 
- build-essential libssl-dev zlib1g-dev 
- linux-headers-generic 
- libcurl4-openssl-dev 
- libxml2-dev libyaml-dev
- tree 
- libcurl4-openssl-dev libssl-dev gcc-4.4 libxslt-dev 
+packages = %w{
+ tree
  emacs23
  imagemagick
- nmap
- libsasl2-2 libsasl2-dev libsasl2-modules
- libcurl4-gnutls-dev libxml2 libxml2-dev libxslt1-dev
 }
 
 packages.each do |pkg|
@@ -24,4 +18,3 @@ packages.each do |pkg|
     action :install
   end
 end
-
