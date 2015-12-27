4f40d87e331f3a195b260667438d4ad1a7e05ee1
Victor Piousbox <piousbox@gmail.com>
2015-12-26 21:29:05 -0800

    0.2.0 version uses hella environments in chef.

--- metadata.rb
+++ metadata.rb
@@ -1,13 +1,11 @@
 maintainer        "maintainer"
-maintainer_email  "314658@gmail.com"
-license           "GNU II"
-description       "Installs"
+maintainer_email  "piousbox@gmail.com"
+license           "MIT"
+description       "Does a bunch of stuff."
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version           "0.1.0"
+version           "0.2.0"
 
 recipe "ish", "Installs"
-recipe "ish::db_bootstrap", "Bootstraps db"
-recipe "ish::extra", "extra stuff for the OS"
 
 %w{ git sqlite nginx mysql application ruby_build rbenv }.each do |cb|
   depends cb
@@ -16,5 +14,3 @@ end
 %w{ ubuntu debian }.each do |os|
   supports os
 end
-
-# version 0.1.0, mysql gem is installed at all times
--- recipes/upstream_rails.rb
+++ recipes/upstream_rails.rb
@@ -1,11 +1,19 @@
 
-gems = %w{ bundler }
+#
+# cookbook ish
+# recipe   upstream_rails
+#
+# _vp_ 20151226
+#
 
-gems.each do |gem|
-  gem_package gem do
-    action :install
-  end
-end
+
+
+# gems = %w{ bundler }
+# gems.each do |gem|
+#   gem_package gem do
+#     action :install
+#   end
+# end
 
 def puts! a, b=""
   puts "+++ +++ #{b}"
@@ -21,56 +29,59 @@ search(:apps) do |any_app|
     if any_app['id'] == role
       app = data_bag_item('apps', any_app['id'])
       if app['type'][app['id']].include?( "upstream_rails" )
-        puts! "Deploying upstream_rails app #{app['id']}"
+        puts! "Deploying ish::upstream_rails app #{app['id']}"
+
+        deploy_to = "/home/#{app['user'][node.chef_environment]}/projects/#{app['id']}"
+        owner = app['owner'][node.chef_environment]
 
         app['packages'].each do |package, version|
           execute "apt-get install #{package} -y"
         end
 
-        directory "#{app['deploy_to']}/shared" do
+        directory "#{deploy_to}/shared" do
           action :create
-          owner app['owner']
+          owner owner
           recursive true
           mode '0766'
         end
         
-        directory "#{app['deploy_to']}/shared/config" do
+        directory "#{deploy_to}/shared/config" do
           action :create
           recursive true
-          owner app['owner']
+          owner owner
         end
         %w{ log pids }.each do |name|
-          directory "#{app['deploy_to']}/shared/#{name}" do
+          directory "#{deploy_to}/shared/#{name}" do
             action :create
             recursive true
-            owner app['owner']
+            owner owner
           end
         end
         
-        execute "install bundler" do
-          command "apt-get install bundler -y"
-        end
+        # execute "install bundler" do
+        #   command "apt-get install bundler -y"
+        # end
         
         #
         # wrapper
         #
         ruby_block "write_key" do
           block do
-            f = ::File.open("#{app['deploy_to']}/id_deploy", "w")
+            f = ::File.open("#{deploy_to}/id_deploy", "w")
             f.print(app["deploy_key"])
             f.close
           end
-          not_if do ::File.exists?("#{app['deploy_to']}/id_deploy"); end
+          not_if do ::File.exists?("#{deploy_to}/id_deploy"); end
         end
-        file "#{app['deploy_to']}/id_deploy" do
-          owner app['owner']
-          group app['group'] 
+        file "#{deploy_to}/id_deploy" do
+          owner owner
+          group owner
           mode '0600'
         end
-        template "#{app['deploy_to']}/deploy-ssh-wrapper" do
+        template "#{deploy_to}/deploy-ssh-wrapper" do
           source "deploy-ssh-wrapper.erb"
-          owner app['owner']
-          group app['group']
+          owner owner
+          group owner
           mode "0755"
           variables app.to_hash
         end
@@ -81,44 +92,45 @@ search(:apps) do |any_app|
         deploy_revision app['id'] do
           revision app['revision'][node.chef_environment]
           repository app['repository']
-          user app['owner']
-          group app['group']
-          deploy_to app['deploy_to']
+          user owner
+          group owner
+          deploy_to deploy_to
           environment 'RAILS_ENV' => app['rack_environment']
           action app['force'][node.chef_environment] ? :force_deploy : :deploy
-          ssh_wrapper "#{app['deploy_to']}/deploy-ssh-wrapper" if app['deploy_key']
+          ssh_wrapper "#{deploy_to}/deploy-ssh-wrapper" if app['deploy_key']
           shallow_clone false # reference is not a tree -> set this to false.
           migrate false
         end
 
-        if app['skip_bundle']
-          ; # do nothing
-        else
-          #
-          # bundle
-          #
-          [ :delete, :create ].each do |which_action|
-            directory "#{app['deploy_to']}/current/vendor" do
-              action which_action
-              recursive true
-            end
-          end
-          execute "bundle" do
-            command "export LANG=en_US.UTF-8 &&
-                     export LANGUAGE=en_US.UTF-8 &&
-                     export export LC_ALL=en_US.UTF-8 && 
-                     bundle --without development test"
-            cwd "#{app['deploy_to']}/current"
-          end
-        end
+        # if app['skip_bundle']
+        #   ; # do nothing
+        # else
+        #   #
+        #   # bundle
+        #   #
+        #   [ :delete, :create ].each do |which_action|
+        #     directory "#{deploy_to}/current/vendor" do
+        #       action which_action
+        #       recursive true
+        #     end
+        #   end
+        #   execute "bundle" do
+        #     command "export LANG=en_US.UTF-8 &&
+        #              export LANGUAGE=en_US.UTF-8 &&
+        #              export export LC_ALL=en_US.UTF-8 && 
+        #              bundle --without development test"
+        #     cwd "#{app['deploy_to']}/current"
+        #   end
+        # end
 
         
         #
         # configure the app
         #
         if app['s3_key']
-          template "#{app['deploy_to']}/current/config/initializers/s3.rb" do
-            owner app['owner']
+          template "#{deploy_to}/current/config/initializers/s3.rb" do
+            owner owner
+            group owner
             source "app/config/initializers/s3.rb.erb"
             variables(
               :key => app['s3_key'],
@@ -128,19 +140,21 @@ search(:apps) do |any_app|
           end
         end
         if app['databases']['mongoid']
-          template "#{app['deploy_to']}/current/config/mongoid.yml" do
-            owner app['owner']
+          template "#{deploy_to}/current/config/mongoid.yml" do
+            owner owner
+            group owner
             source "app/config/mongoid.yml.erb"
             variables(
-              :host => app['databases']['mongoid']['host'],
-              :database => app['databases']['mongoid']['database'],
+              :host        => app['databases']['mongoid'][node.chef_environment]['host'],
+              :database    => app['databases']['mongoid'][node.chef_environment]['database'],
               :environment => app['rack_environment']
             )
           end
         end
         if app['recaptcha_site_key']
-          template "#{app['deploy_to']}/current/config/initializers/recaptcha.rb" do
-            owner app['owner']
+          template "#{deploy_to}/current/config/initializers/recaptcha.rb" do
+            owner owner
+            group owner
             source "app/config/initializers/recaptcha.rb.erb"
             variables(
               :site_key => app['recaptcha_site_key'],
@@ -149,12 +163,13 @@ search(:apps) do |any_app|
           end
         end
         if %w( mysql mysql2 ).include? app['databases'][node.chef_environment]['adapter']
-          template "#{app['deploy_to']}/shared/config/database.yml" do
-            owner app['owner']
+          template "#{deploy_to}/shared/config/database.yml" do
+            owner owner
+            group owner
             source "app/config/database_remote.yml.erb"
             variables(
               :database => app['databases'][node.chef_environment]['database'],
-              :host => app['databases'][node.chef_environment]['host'],
+              :host     => app['databases'][node.chef_environment]['host'],
               :username => app['databases'][node.chef_environment]['username'],
               :password => app['databases'][node.chef_environment]['password']
             )
@@ -165,10 +180,11 @@ search(:apps) do |any_app|
         # create some dirs
         #
         %w{ tmp tmp/cache tmp/cache/assets }.each do |folder|
-          directory "#{app['deploy_to']}/current/#{folder}" do
+          directory "#{deploy_to}/current/#{folder}" do
             action :create
             mode '0777'
-            owner app['owner']
+            owner owner
+            group owner
             recursive true
           end
         end
@@ -177,17 +193,17 @@ search(:apps) do |any_app|
         #
         # service
         #
-        template "#{app['deploy_to']}/shared/unicorn.rb" do
-          owner app['owner']
-          group app['owner']
+        template "#{deploy_to}/shared/unicorn.rb" do
+          owner owner
+          group owner
           source "unicorn.conf.rb.erb"
           mode "0664"
           variables({
                       :app => app['id'],
-                      :deploy_to => app['deploy_to'],
+                      :deploy_to => deploy_to,
                       :port => app['unicorn_port'],
-                      :owner => app['owner'],
-                     :group => app['group']
+                      :owner => owner,
+                      :group => owner
                     })
         end
         upstart_script_name = "#{app['id']}-app"
@@ -198,12 +214,12 @@ search(:apps) do |any_app|
           mode "0664"
           variables(
             :app_name       => app['id'],
-            :app_root       => "#{app['deploy_to']}/current",
-            :log_file       => "#{app['deploy_to']}/current/log/unicorn.log",
-            :unicorn_config => "#{app['deploy_to']}/shared/unicorn.rb",
+            :app_root       => "#{deploy_to}/current",
+            :log_file       => "#{deploy_to}/current/log/unicorn.log",
+            :unicorn_config => "#{deploy_to}/shared/unicorn.rb",
             :unicorn_binary => "bundle exec unicorn_rails",
-            :rack_env       => 'production',
-            :user           => app['owner']
+            :rack_env       => app['rack_environment'],
+            :user           => owner
           )
         end
         service upstart_script_name do
