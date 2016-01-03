9c98036b3e14bc1aae5bcea51221cd1b24009dac
Victor Piousbox <piousbox@gmail.com>
2016-01-02 23:59:00 -0800

    speced mysql installation

--- spec/recipes/install_mysql_spec.rb
+++ spec/recipes/install_mysql_spec.rb
@@ -8,9 +8,21 @@ describe 'ish::install_mysql' do
     end.converge(described_recipe)
   end
 
-  it 'leverages the mysql cookbook' do
-    expect(chef_run).to create_mysql_service "default"
-    expect(chef_run).to start_mysql_service "default"
+  it 'pre-sets password' do
+    expect(chef_run).to run_execute "pre-set password"
+  end
+
+  it 'installs mysql package' do
+    expect(chef_run).to install_package "mysql-server"
+  end
+
+  it 'adjusts bind address' do
+    expect(chef_run).to run_execute "adjust bind address"
+  end
+
+  it 'enables, starts service' do
+    expect(chef_run).to enable_service 'mysql'
+    expect(chef_run).to start_service 'mysql'
   end
   
 end
